d3eaeb87565d151fbd4ad172e355edd45f30b525
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2025-05-05 15:10:20 +0600

    build 9

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
-module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-    '@tailwindcss/postcss',
-  ],
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 };
+export default config;
